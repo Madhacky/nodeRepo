4868b45ea0433022c87eb5c4c99ed19607c1e24d
madhacky <mdshahjada747@gmail.com>
2023-04-25 17:56:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, "192.169.1.198", () => {
+  console.log(`Server Started at ${PORT}`);
+});
+require("dotenv").config();
+
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+const routes = require("./routes/routes");
+app.use("/api", routes);
